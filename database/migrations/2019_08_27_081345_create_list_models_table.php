627811ca1a4834eab151932fe26828191982b1d7
Adam Reyner <reyneradam@gmail.com>
2019-09-11 11:11:23 +0700

    update dikit

--- database/migrations/2019_08_27_081345_create_list_models_table.php
+++ database/migrations/2019_08_27_081345_create_list_models_table.php
@@ -16,7 +16,7 @@ class CreateListModelsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('product_type');
-            $table->integer('category_id');
+            $table->integer('categories_id');
             $table->string('product_name');
             $table->integer('base_price');
             $table->integer('final_price');
